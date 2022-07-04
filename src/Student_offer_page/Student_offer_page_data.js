eca512bb58afcc7aaf57e32e3c2fb8559c2a42ec
Ujjawal-kumar19 <kumar9ujjawal2014@gmail.com>
2022-07-04 19:36:09 +0530

    few updates

--- src/Student_offer_page/Student_offer_page_data.js
+++ src/Student_offer_page/Student_offer_page_data.js
@@ -35,6 +35,84 @@ const Student_offer_page_data = [
     link: "https://education.github.com/pack",
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png",
   },
+  {
+    title: "HP",
+    body: "Gadgets.",
+    link: "https://www.hp.com/in-en/shop/students/about-the-program",
+    img: "https://in-media.apjonlinecdn.com/logo/stores/7/HP_Logo_Halo.png",
+  },
+  {
+    title: "Lenovo",
+    body: "Gadgets.",
+    link: "https://www.lenovo.com/in/en/deals/back-to-school-sale/back-to-school-laptops/?orgRef=https%253A%252F%252Fsearch.yahoo.com%252F",
+    img: "https://brandpalettes.com/wp-content/uploads/2018/10/Lenovo-Logo.png",
+  },
+  {
+    title: "Microsoft",
+    body: "Gadgets & Software.",
+    link: "https://www.microsoft.com/en-gd/store/b/student",
+    img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31",
+  },
+  {
+    title: "Samsung",
+    body: "Gadgets.",
+    link: "https://www.samsung.com/in/microsite/student-advantage/",
+    img: "https://i.pinimg.com/originals/74/79/a0/7479a0a02cf333e01e13d1b6d08af800.jpg",
+  },
+  {
+    title: "IndiGo",
+    body: "This discount entitles you with up to 6% off on base fare and an extra baggage allowance of 10kg.",
+    link: "https://www.goindigo.in/add-on-services/student-discount.html",
+    img: "https://1000logos.net/wp-content/uploads/2021/07/IndiGo-Logo-768x483.png",
+  },
+  {
+    title: "Spicejet",
+    body: "Discout upto 10% and allowance to carry 10kg of extra baggage free.",
+    link: "https://corporate.spicejet.com/studentdiscountlanding.aspx",
+    img: "https://1000logos.net/wp-content/uploads/2021/07/SpiceJet-Logo-768x483.png",
+  },
+  {
+    title: "FlyGofirst",
+    body: " Get 5% discount on base fare and baggage allowance up to 25 kg. Enjoy this bagful of privileges right away!.",
+    link: "https://www.flygofirst.com/offers-and-promotions/student-offer/",
+    img: "https://pbs.twimg.com/profile_images/1486631662121209858/fjcYA38M_400x400.jpg",
+  },
+  {
+    title: "Vistara",
+    body: "Enjoy up to 10% off and extra baggage allowance with Vistara Student Discount.",
+    link: "https://www.airvistara.com/in/en/vistara-exclusives/vistara-student-discount",
+    img: "http://www.airline-empires.com/uploads/gallery/album_3977/gallery_295130_3977_772440.png",
+  },
+  {
+    title: "Evernote",
+    body: "Eligible college students can get 50% off a one-year subscription to Evernote Personal.",
+    link: "https://help.evernote.com/hc/en-us/articles/360043733934-Student-Offer-50-off-Personal-Eligible-Domains",
+    img: "https://www.logo-designer.co/wp-content/uploads/2018/08/2018-evernote-new-logo-design-branding.png",
+  },
+  {
+    title: "Canva",
+    body: "If you qualify for the GitHub Student Developer Pack, you can claim a free year of Canva Pro.",
+    link: "https://www.canva.com/education/github/",
+    img: "https://logos-world.net/wp-content/uploads/2021/11/Canva-Logo-700x394.png",
+  },
+  {
+    title: "Shillshare",
+    body: "50% off one year of membership for college & university students.",
+    link: "https://www.skillshare.com/scholarships",
+    img: "https://static.skillshare.com/assets/images/opengraph-default.png",
+  },
+  {
+    title: "Spotify",
+    body: "Variety of student discounts avaiable.",
+    link: "https://www.spotify.com/in-en/student/",
+    img: "https://www.magneticmag.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1280/MTQzNDkwNDU1MDE3ODkwOTU5/spotify-1360002_1280jpg.webp",
+  },
+  {
+    title: "Dell",
+    body: "Instant discounts up to ₹5,000 on laptops of your choice.",
+    link: "https://www.dell.com/en-in/lp/student-laptop-discount",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Dell_Logo.svg/600px-Dell_Logo.svg.png?20160807135325",
+  },
 ];
 
 export default Student_offer_page_data;
